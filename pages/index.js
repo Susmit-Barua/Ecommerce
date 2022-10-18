8d75b0c627e21a9d677c565f62a98978d6b72a04
Susmit Barua <111695274+Susmit-Barua@users.noreply.github.com>
2022-10-18 12:37:01 +0600

    Developing

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { client } from "../lib/client";
+import { Product, HeroBanner, FooterBanner } from "../components";
 
-const index = () => {
-  return <div>index</div>;
+const Home = ({ products, bannerData }) => {
+  return (
+    <>
+      <HeroBanner heroBanner={bannerData[0]} />
+      {console.log(products)}
+      <div>
+        <h2 className="products-heading">Best selling products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+      <div className="products-container">
+        {products.map((product) => product.name)}
+      </div>
+      <FooterBanner />
+    </>
+  );
 };
+export const getServerSideProps = async () => {
+  const query = '*[_type=="product"]';
+  const products = await client.fetch(query);
 
-export default index;
+  const bannerquery = '*[_type=="banner"]';
+  const bannerData = await client.fetch(bannerquery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
+export default Home;
